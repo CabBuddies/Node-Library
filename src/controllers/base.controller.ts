15f19ca3fcd66cdd79406eb4713b17abb06c3d1a
Nihal Konda <nihal.konda@sjsu.edu>
2020-09-10 11:02:10 -0700

    [Fixed] BaseController

--- src/controllers/base.controller.ts
+++ src/controllers/base.controller.ts
@@ -14,66 +14,115 @@ class BaseController implements Controler{
     }
 
     create : any = async(req : express.Request , res : express.Response) => {
-        const request : Request = res.locals.request;
-        const body = req.body; 
+        try {
+            const request : Request = res.locals.request;
+            const body = req.body; 
 
-        const createdEntity = await this.service.create(request, body);
+            const createdEntity = await this.service.create(request, body);
 
-        return res.send(createdEntity);
+            return res.send(createdEntity);
+        } catch (error) {
+            if(error.status && error.message){
+                return res.status(error.status).send(error.message);
+            }
+            return res.status(500).send('unknown server issue');
+        }
     } 
 
     get : any = async(req : express.Request , res : express.Response) => {
-        const request : Request = res.locals.request;
-        const entityId = req.params.id; 
-        const entity = await this.service.get(request, entityId);
-        return res.send(entity);
+        try {
+            const request : Request = res.locals.request;
+            const entityId = req.params.id; 
+            const entity = await this.service.get(request, entityId);
+            return res.send(entity);
+        } catch (error) {
+            if(error.status && error.message){
+                return res.status(error.status).send(error.message);
+            }
+            return res.status(500).send('unknown server issue');
+        }
     }
 
     getAll : any = async(req : express.Request , res : express.Response) => {
-        const request : Request = res.locals.request;
+        try {
+            const request : Request = res.locals.request;
 
-        const pageSize :number = parseInt(req.query.pageSize+'') || 5
-        const pageNum :number = parseInt(req.query.pageNum+'') || 1
-        
-        const query = req.body.query
+            const pageSize :number = parseInt(req.query.pageSize+'') || 5
+            const pageNum :number = parseInt(req.query.pageNum+'') || 1
+            
+            const query = req.body.query
 
-        const attributes = undefined
+            const attributes = undefined
 
-        const result = await this.service.getAll(request, query, pageSize, pageNum, attributes);
-        return res.send(result);
+            const result = await this.service.getAll(request, query, pageSize, pageNum, attributes);
+            return res.send(result);
+        } catch (error) {
+            if(error.status && error.message){
+                return res.status(error.status).send(error.message);
+            }
+            return res.status(500).send('unknown server issue');
+        }
     }
 
     update : any = async(req : express.Request , res : express.Response) => {
-        const request : Request = res.locals.request;
-        const entityId = req.params.id; 
-        const body = req.body; 
+        try {
+            const request : Request = res.locals.request;
+            const entityId = req.params.id; 
+            const body = req.body; 
 
-        const updatedEntity = await this.service.update(request, entityId, body);
+            const updatedEntity = await this.service.update(request, entityId, body);
 
-        return res.send(updatedEntity);
+            return res.send(updatedEntity);
+        } catch (error) {
+            if(error.status && error.message){
+                return res.status(error.status).send(error.message);
+            }
+            return res.status(500).send('unknown server issue');
+        }
     }
 
     updatePartial : any = async(req : express.Request , res : express.Response) => {
-        const request : Request = res.locals.request;
-        const entityId = req.params.id; 
-        const body = req.body; 
+        try {
+            const request : Request = res.locals.request;
+            const entityId = req.params.id; 
+            const body = req.body; 
 
-        const updatedEntity = await this.service.updatePartial(request, entityId, body);
+            const updatedEntity = await this.service.updatePartial(request, entityId, body);
 
-        return res.send(updatedEntity);
+            return res.send(updatedEntity);
+        } catch (error) {
+            if(error.status && error.message){
+                return res.status(error.status).send(error.message);
+            }
+            return res.status(500).send('unknown server issue');
+        }
     }
 
     delete : any = async(req : express.Request , res : express.Response) => {
-        const request : Request = res.locals.request;
-        const entityId = req.params.id; 
-        const deletedEntity = await this.service.delete(request, entityId);
-        return res.send(deletedEntity);
+        try {
+            const request : Request = res.locals.request;
+            const entityId = req.params.id; 
+            const deletedEntity = await this.service.delete(request, entityId);
+            return res.send(deletedEntity);
+        } catch (error) {
+            if(error.status && error.message){
+                return res.status(error.status).send(error.message);
+            }
+            return res.status(500).send('unknown server issue');
+        }
     }
 
     deleteAll : any = async(req : express.Request , res : express.Response) => {
-        const request : Request = res.locals.request;
-        const deletedEntities = await this.service.deleteAll(request);
-        return res.send(deletedEntities);
+        try {
+            const request : Request = res.locals.request;
+            const deletedEntities = await this.service.deleteAll(request);
+            return res.send(deletedEntities);
+        } catch (error) {
+            if(error.status && error.message){
+                return res.status(error.status).send(error.message);
+            }
+            return res.status(500).send('unknown server issue');
+        }
     }
 
 }
