44c309a3de70cb8a9d5365c20410b7eb46836749
Nihal Konda <nihal.konda@sjsu.edu>
2020-09-11 13:00:21 -0700

    [Built] Binder

--- lib/services/index.d.ts
+++ lib/services/index.d.ts
@@ -1,4 +1,5 @@
 import Service from './service';
 import BaseService from './base.service';
 import * as PubSub from './pubsub';
-export { Service, BaseService, PubSub };
+import Binder from './binder';
+export { Service, BaseService, PubSub, Binder };
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,11 @@
 import Service from './service';
 import BaseService from './base.service';
 import * as PubSub from './pubsub';
+import Binder from './binder';
 
 export {
     Service,
     BaseService,
-    PubSub
+    PubSub,
+    Binder
 }
