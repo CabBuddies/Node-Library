0daab3a0ff78f72ad639f7d8072513a1fb8f3981
Nihal Konda <nihal.konda@sjsu.edu>
2020-09-12 21:52:16 -0700

    [Built] ValidatorMiddleware

---
+++ lib/middlewares/validator.middleware.d.ts
@@ -0,0 +1,28 @@
+import * as express from 'express';
+interface ValidationTypeSchema {
+    name: string;
+    data?: any;
+    type: string;
+}
+interface ValidationSchema extends ValidationTypeSchema {
+    array_defaultValue?: [any];
+    array_item_type?: any;
+    array_min?: number;
+    array_max?: number;
+    array_size?: number;
+    array_unique?: boolean;
+    array_normalize?: boolean;
+    defaultValue?: any;
+    min?: number;
+    max?: number;
+    size?: number;
+    regex?: string;
+    trim?: boolean;
+    lower?: boolean;
+    upper?: boolean;
+    equal?: any;
+    anyOf?: [any];
+    optional?: boolean;
+}
+export default function validateRequestBody(schemas: [ValidationSchema]): (req: express.Request, res: express.Response, next: express.NextFunction) => express.Response<any>;
+export {};
