1c8ae7b301a0b7fe7df1fdb6320976c80d7b5f67
Nihal Konda <nihal.konda@sjsu.edu>
2020-09-16 19:34:44 -0700

    [Fixed] BaseRepository

--- src/repositories/base.repository.ts
+++ src/repositories/base.repository.ts
@@ -8,12 +8,12 @@ class BaseRepository implements Respository{
         this.model = model;
     }
 
-    get = async(documentId:string ,attributes = {password:0}) => {
+    async get(documentId:string ,attributes = {password:0}) {
         console.log('base.repository ',documentId)
         return await this.model.findById(documentId).select(attributes);
     }
 
-    getAll = async(query = {}, sort = {}, pageSize : number = 5, pageNum : number = 1, attributes:any={'password':0}) => {
+    async getAll(query = {}, sort = {}, pageSize : number = 5, pageNum : number = 1, attributes:any={'password':0}) {
         //skip - limit
         console.log('base.repository',query,sort,attributes,pageSize,pageNum);
         const skips = pageSize * (pageNum - 1)
@@ -37,23 +37,23 @@ class BaseRepository implements Respository{
         }
     }
 
-    create = async(document:any) => {
+    async create(document:any) {
         return await this.model.create(document);
     }
 
-    update = async(documentId:string , document) => {
+    async update(documentId:string , document) {
         return await this.model.findByIdAndUpdate(documentId, document, {new: true});
     }
 
-    updatePartial = async(documentId:string , partial:any) => {
+    async updatePartial(documentId:string , partial:any) {
         return await this.model.findByIdAndUpdate(documentId, {$set:partial}, {new: true});
     }
 
-    delete = async(documentId:string) => {
+    async delete(documentId:string) {
         return await this.model.findByIdAndDelete(documentId);
     }
 
-    deleteAll = async() => {
+    async deleteAll() {
         return await this.model.deleteMany({});
     }
 }
