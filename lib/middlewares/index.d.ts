785a2306d9dc1a72e6b552dc68131caae4231f6b
Nihal Konda <nihal.konda@sjsu.edu>
2020-10-15 04:00:00 -0700

    [Built] Request Middleware

--- lib/middlewares/index.d.ts
+++ lib/middlewares/index.d.ts
@@ -1,5 +1,5 @@
 import authCheck from './auth.middleware';
 import { logger } from './logger.middleware';
-import { requestProcessor } from './request.middleware';
+import { requestProcessor, addParamToRequest } from './request.middleware';
 import ValidatorMiddleware from './validator.middleware';
-export { authCheck, logger, requestProcessor, ValidatorMiddleware };
+export { authCheck, logger, requestProcessor, addParamToRequest, ValidatorMiddleware };
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,11 +1,12 @@
 import authCheck from './auth.middleware';
 import {logger} from './logger.middleware';
-import {requestProcessor} from './request.middleware';
+import {requestProcessor,addParamToRequest} from './request.middleware';
 import ValidatorMiddleware from './validator.middleware';
 
 export {
     authCheck,
     logger,
     requestProcessor,
+    addParamToRequest,
     ValidatorMiddleware
 };
