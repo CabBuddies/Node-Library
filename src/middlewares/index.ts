facf16c7c16d471e7afa65ff74d6d61fecc50b51
Nihal Konda <nihal.konda@sjsu.edu>
2020-11-04 18:29:35 -0800

    [Built] Author Middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,10 +1,12 @@
 import authCheck from './auth.middleware';
+import isAuthor from './author.middleware';
 import {logger} from './logger.middleware';
 import {requestProcessor,addParamToRequest} from './request.middleware';
 import ValidatorMiddleware from './validator.middleware';
 
 export {
     authCheck,
+    isAuthor,
     logger,
     requestProcessor,
     addParamToRequest,
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,11 +1,13 @@
 import Service from './service';
 import BaseService from './base.service';
+import AuthorService from './author.service';
 import * as PubSub from './pubsub';
 import Binder from './binder';
 
 export {
     Service,
     BaseService,
+    AuthorService,
     PubSub,
     Binder
 }
