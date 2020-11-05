af62350ecb2e4ff750b1e4a66b1d30c923439983
Nihal Konda <nihal.konda@sjsu.edu>
2020-11-04 18:33:56 -0800

    [Built] Doc Exists Middleware

--- lib/middlewares/index.d.ts
+++ lib/middlewares/index.d.ts
@@ -3,4 +3,5 @@ import isAuthor from './author.middleware';
 import { logger } from './logger.middleware';
 import { requestProcessor, addParamToRequest } from './request.middleware';
 import ValidatorMiddleware from './validator.middleware';
-export { authCheck, isAuthor, logger, requestProcessor, addParamToRequest, ValidatorMiddleware };
+import checkDocumentExists from './document.exists.middleware';
+export { authCheck, isAuthor, checkDocumentExists, logger, requestProcessor, addParamToRequest, ValidatorMiddleware };
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,10 +3,12 @@ import isAuthor from './author.middleware';
 import {logger} from './logger.middleware';
 import {requestProcessor,addParamToRequest} from './request.middleware';
 import ValidatorMiddleware from './validator.middleware';
+import checkDocumentExists from './document.exists.middleware';
 
 export {
     authCheck,
     isAuthor,
+    checkDocumentExists,
     logger,
     requestProcessor,
     addParamToRequest,
