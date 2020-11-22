79d77b2d1fa3f53ed8dba2ce0c333b1845326806
Nihal Konda <nihal.konda@sjsu.edu>
2020-11-22 00:00:40 -0800

    [Fixed] AuthMiddlewares

--- lib/middlewares/auth.middleware.d.ts
+++ lib/middlewares/auth.middleware.d.ts
@@ -1,3 +1,3 @@
 import * as express from 'express';
 import Request from '../helpers/request.helper';
-export default function authCheck(required?: boolean, hasToBeConfirmed?: boolean, isRefresh?: boolean): (req: express.Request, res: express.Response, next: express.NextFunction) => express.Response<any>;
+export default function authCheck(required?: boolean, hasToBeConfirmed?: boolean, hasToBeRefresh?: boolean): (req: express.Request, res: express.Response, next: express.NextFunction) => express.Response<any>;
