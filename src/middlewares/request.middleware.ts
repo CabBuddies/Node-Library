7bd01f0be4e369f237888d741a2a1234af77e030
Nihal Konda <nihal.konda@sjsu.edu>
2020-11-21 23:25:37 -0800

    [Fixed] RequestMiddlewares

--- src/middlewares/request.middleware.ts
+++ src/middlewares/request.middleware.ts
@@ -63,6 +63,7 @@ function requestProcessor (service : Service = null){
                         }
                     } catch (error) {
                         console.log('middleware','requestProcessor',error);
+                        activeTokenCount=0;
                     }
     
                     console.log('requestProcessor','activeTokenCount',activeTokenCount);
